1091982c4fb329872c888a79a3779b181ce748d8
Ruslan <ruslanzakharov3008@gmail.com>
2022-08-13 01:32:49 +0300

    Сделан набросок по добавлению слов

---
+++ bot/handlers.py
@@ -0,0 +1,70 @@
+import telegram as tg
+import telegram.ext as tg_ext
+
+START, ENG_WORD, RU_WORD = range(3)
+
+
+async def start(update: tg.Update, context: tg_ext.ContextTypes.DEFAULT_TYPE) -> int:
+    keyboard = [['Новое слово', 'Старое слово']]
+    reply_markup = tg.ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text('ПРИВЕТ!!!')
+    await update.message.reply_text(
+        f'Привет, {update.effective_user.first_name}. Я помогу тебе запомнить как можно больше иностранных слов!',
+        reply_markup=reply_markup
+    )
+    return START
+
+
+async def new_word(update: tg.Update, context: tg_ext.ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text('Введите слово на английском')
+    print('new_word сработало')
+    return ENG_WORD
+
+
+async def new_word_eng(update: tg.Update, context: tg_ext.ContextTypes.DEFAULT_TYPE) -> int:
+    eng_word = update.message.text
+    context.user_data['eng_word'] = update.message.text
+
+    await update.message.reply_text('Введите слово на русском')
+
+    return RU_WORD
+
+
+async def new_word_ru(update: tg.Update, context: tg_ext.ContextTypes.DEFAULT_TYPE) -> int:
+    ru_word = update.message.text
+
+    print(context.user_data['eng_word'], ru_word)
+
+    keyboard = [['Новое слово']]
+    reply_markup = tg.ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        'Добавлено!',
+        reply_markup=reply_markup
+    )
+
+    return START
+
+
+def setup_handlers(application: tg_ext.Application) -> None:
+    # start_handler = tg_ext.CommandHandler('start', start)
+
+    conv_handler = tg_ext.ConversationHandler(
+        entry_points=[tg_ext.CommandHandler('start', start)],
+        states={
+            START: [
+                tg_ext.MessageHandler(tg_ext.filters.Regex('^(Новое слово|Старое слово)$'), new_word)
+            ],
+            ENG_WORD: [
+                tg_ext.MessageHandler(tg_ext.filters.TEXT, new_word_eng)
+            ],
+            RU_WORD: [
+                tg_ext.MessageHandler(tg_ext.filters.TEXT, new_word_ru)
+            ],
+        },
+        fallbacks=[tg_ext.CommandHandler('start', start)]
+    )
+
+    # application.add_handler(start_handler)
+    application.add_handler(conv_handler)
--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import os
 
 import telegram.ext as tg_ext
 
+from bot import handlers
+
 TOKEN = os.getenv('TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+def main() -> None:
     application = tg_ext.ApplicationBuilder().token(TOKEN).build()
 
+    handlers.setup_handlers(application)
+
     application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
